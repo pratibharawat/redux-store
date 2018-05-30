edf57d41115d968c5c3c3ee6f9c3624210742a08
Pratibha Rawat <prawat@amazon.com>
2018-05-29 19:39:20 -0700

    Setting up the redux store

---
+++ index.js
@@ -0,0 +1,13 @@
+function createStore() {
+    // the store have four parts:
+    // the state
+    // get the state
+    // listen to changes in state
+    // update the state
+
+    let state;
+    const getState = () => state
+        return {
+        getState
+    };  
+}
