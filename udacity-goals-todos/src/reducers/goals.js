2b9cd7192ad4ddf43a78d977d70d0d738ad32761
Pratibha Rawat <prawat@amazon.com>
2018-07-05 18:29:47 -0700

    Adding the store

--- udacity-goals-todos/src/reducers/goals.js
+++ udacity-goals-todos/src/reducers/goals.js
@@ -2,6 +2,10 @@ import {
     ADD_GOAL,
     REMOVE_GOAL
   } from '../actions/goals'
+
+  import {
+    RECEIVE_DATA
+  } from '../actions/shared'
   
   export default function goals (state = [], action) {
     switch(action.type) {
